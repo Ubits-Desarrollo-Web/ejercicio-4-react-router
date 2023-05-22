6bb7432bebbc8126c6d229769ce8278a55080ee2
Miguel Bacigaluppi <miguebaci@gmail.com>
2023-05-22 10:50:44 -0300

    solucion

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Inicio = () => {
     return <div className="Inicio">
-            {/*<button className="button-styled">1</button>*/}
-        </div>;
+        <Link to={'numeros/1'}>
+            <button className="button-styled">1</button>
+        </Link>
+        <Link to={'numeros/2'}>
+            <button className="button-styled">2</button>
+        </Link>
+        <Link to={'numeros/3'}>
+            <button className="button-styled">3</button>
+        </Link>
+    </div>;
 }
 
 export default Inicio;
